5f08832c19c82f4777436e4e2f0ef2be5e20fd41
burakkaratasz <burakkaratasz@outlook.com>
2024-02-23 14:26:09 +0300

    first commit

---
+++ app/src/main/java/com/example/simplenoteapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.simplenoteapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.simplenoteapp.ui.theme.SimpleNoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnrememberedMutableState")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleNoteAppTheme {
+                var count by remember{
+                    mutableStateOf(0)
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+
+                ) {
+                    Text(
+                        text = count.toString(),
+                        fontSize = 30.sp
+                    )
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Click Me!")
+                    }
+                }
+            }
+        }
+    }
+}
